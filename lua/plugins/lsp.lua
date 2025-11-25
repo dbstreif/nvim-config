b0f37c19559c9075cde808796e78d0e388f12abe
Dominic Streif <streif.dominic@gmail.com>
2025-11-25 03:26:14 -0800

    removed pyright because it was causing problems

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "lua_ls", "rust_analyzer", "pyright" },
+        ensure_installed = { "lua_ls", "rust_analyzer" },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
